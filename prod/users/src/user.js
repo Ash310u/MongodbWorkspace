9a2659bef063e19e4da4d4c367005045e08e7b06
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-09-14 12:33:12 +0530

    Commited a comment related to mongoose.Schema

---
+++ prod/users/src/user.js
@@ -0,0 +1,4 @@
+const  mongoose  = require("mongoose");
+// Schema is the thing that says I expect my model to have a "this" property and it should follow these validities
+const Schema = mongoose.Schema;
+
