e37cd61440c97f7546b984a1f51ef4640fe41c41
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-09-16 01:49:22 +0530

    Fixed the required User bug

--- prod/users/test/create_test.js
+++ prod/users/test/create_test.js
@@ -1,8 +1,9 @@
 const assert = require('assert')
-const User = require('../src/user')
+const { User }= require('../src/user')
 
 describe('Creating records', () => {
     it('saves a user', () => {
-        const jeo = new User ({ name:'Joe' })
+        const joe = new User({ name:'Joe' });
+        joe.save();
     })
 })
