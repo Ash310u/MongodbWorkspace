61d6b3a1258a0356e12cd0be8cb8a466c54503c6
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-09-16 05:21:37 +0530

    Commited a assertion for user saved in database by using .isNew

--- prod/users/test/create_test.js
+++ prod/users/test/create_test.js
@@ -1,9 +1,15 @@
 const assert = require('assert')
 const { User }= require('../src/user')
 
-describe('Creating records', () => {
+describe('Creating records', (done) => {
     it('saves a user', () => {
         const joe = new User({ name:'Joe' });
-        joe.save();
+        
+        joe.save()
+            .then(() => {
+                // Has joe been saved successfully?
+                assert(!joe.isNew);
+                done()
+            })
     })
 })
