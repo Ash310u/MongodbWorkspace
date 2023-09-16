05b381b2ee52ed95f39439255652664c60ec880e
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-09-16 07:34:32 +0530

    Wrapped up the connection statement with a before call and passed a 'done' callback and called it for informing mocha that we're ready to go on to first test

--- prod/users/test/test_helper.js
+++ prod/users/test/test_helper.js
@@ -2,12 +2,16 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-mongoose.connect('mongodb://localhost/users_test')
-mongoose.connection
-    .once('open', () => {})
-    .on('error', (error) => {
-        console.warn('Warning', error);
-    });
+before((done) => {
+    mongoose.connect('mongodb://localhost/users_test')
+    mongoose.connection
+        .once('open', () => {
+            done();
+        })
+        .on('error', (error) => {
+            console.warn('Warning', error);
+        });
+})
 
 beforeEach((done) => {
     mongoose.connection.collections.users.drop(() => {
