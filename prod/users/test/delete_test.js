a0380bc33651f55ece99d836aef78ff7d6e98e48
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-09-18 03:27:46 +0530

    Commited many ways to delete records from the database using mongoose methods

---
+++ prod/users/test/delete_test.js
@@ -0,0 +1,51 @@
+const assert = require("assert");
+const { User } = require("../src/user");
+
+describe("Deleting a user", () => {
+    let joe;
+
+    beforeEach((done) => {
+        joe = new User({ name: "joe" });
+        joe.save().then(() => {
+            done();
+        });
+    });
+    
+    it("Class method deleteOne", (done) => {
+        User.deleteOne(joe._id)
+            .then(() => User.findOne({  _id: joe._id }))
+            .then((user) => {
+                assert(user === null)
+                done()
+            })
+    });
+
+    it("Class method deleteMany", (done) => {
+        // Remove a bunch of records with some given criteria
+        User.deleteMany({ name: 'joe' })
+            .then(() => User.findOne({ name: 'joe' }))
+            .then((user) => {
+                assert(user === null)
+                done()
+            })
+    });
+
+    it("Class method findOneAndDelete", (done) => {
+        User.findOneAndDelete({ name:'joe' })
+            .then(() => User.findOne({ name: 'joe' }))
+            .then((user) => {
+                assert(user === null)
+                done()
+            })
+    });
+
+    it("Class method findByIdAndDelete", (done) => {
+        User.findByIdAndDelete(joe._id)
+            .then(() => User.findOne({ _id: joe._id }))
+            .then((user) => {
+                assert(user === null)
+                done()
+            })
+    });
+
+});
