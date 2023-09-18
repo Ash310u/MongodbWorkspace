51f8a07b4c553e876c2c35741390eb97148c9bf4
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-09-18 06:36:37 +0530

    Commited assertName function for making assertion

--- prod/users/test/update_test.js
+++ prod/users/test/update_test.js
@@ -1,23 +1,27 @@
 const assert = require('assert')
-const { User }= require('../src/user')
+const { User } = require('../src/user')
 
 describe('Updating records', () => {
     let joe;
 
     beforeEach((done) => {
-        joe = new User({name:'joe'})
+        joe = new User({ name: 'joe' })
         joe.save()
             .then(() => done())
     })
 
-    it('Instance type using set n save', (done) => {
-        joe.set('name', 'Tom')
-        joe.save()
+    const assertName = (operation, done) => {
+        operation
             .then(() => User.find({}))
             .then((users) => {
                 assert(users.length === 1)
                 assert(users[0].name === 'Tom')
                 done()
             })
+    }
+
+    it('Instance type using set n save', (done) => {
+        joe.set('name', 'Tom')
+        assertName(joe.save(), done)
     })
 })
