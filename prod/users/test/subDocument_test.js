3bc9c03f3bc1d1074d8dc71bdfd859a9d4f89eb0
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-09-21 12:26:01 +0530

    Commited a it block and set a assertion for test of adding subdocument in an existing user

--- prod/users/test/subDocument_test.js
+++ prod/users/test/subDocument_test.js
@@ -5,13 +5,35 @@ describe('Subdocument', () => {
     it('It can create a subdocument', (done) => {
         const joe = new User({
             name: 'joe',
-            posts: [{ title: '1st post' }] 
+            posts: [{ title: '1st post' }]
         })
         joe.save()
-            .then(() => User.findOne({ _id:joe._id }))
+            .then(() => User.findOne({ _id: joe._id }))
             .then((user) => {
                 const { title } = user.posts[0]
                 assert(title === '1st post')
+                done()
+            })
+    });
+
+    it('Can add a subdocument to an existing record', (done) => {
+        // created joe
+        const joe = new User({
+            name: 'joe',
+            posts: []
+        })
+        // saved joe
+        joe.save()      // fetched joe
+            .then(() => User.findOne({ name: 'joe' }))
+            .then((user) => {
+                // Added a post to joe
+                user.posts.push({ title: '1st post' })
+                // saved joe
+                return user.save();
+            })          // fetched the updated joe
+            .then(() => User.findOne({ name:'joe' }))
+            .then((user) => {
+                assert(user.posts[0].title === '1st post')
                 done()
             })
     })
