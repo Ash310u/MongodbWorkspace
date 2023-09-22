ea0a71b3fa3470f1e358c3db33a74a4dd077d0c8
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-09-22 20:44:21 +0530

    Removed some typo

--- prod/users/test/subDocument_test.js
+++ prod/users/test/subDocument_test.js
@@ -1,6 +1,5 @@
 const assert = require('assert')
 const { User } = require('../src/user');
-const { log } = require('console');
 
 describe('Subdocument', () => {
     it('It can create a subdocument', (done) => {
