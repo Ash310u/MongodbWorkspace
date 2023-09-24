d2246c03f32fceeadcc2703445e7342aeb9fe624
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-09-24 09:04:06 +0530

    Commited a test driven development approach for vitual type

--- prod/users/test/virtual_type_test.js
+++ prod/users/test/virtual_type_test.js
@@ -2,5 +2,16 @@ const assert = require('assert')
 const { User } = require('../src/user');
 
 describe('virtual type test', () => {
-
+    it('postCount return number of posts', (done) => {
+        const joe = new User({
+            name:'joe',
+            posts: [{ title:'postCount check' }]
+        })
+        joe.save()
+            .then(() => User.findOne({ name:'joe' }))
+            .then((user) => {
+                assert(user.postcount === 1)
+                done()
+            })
+    })
 })
