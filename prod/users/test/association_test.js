b09964b3df14442201816dd65fbaf375c2cbae49
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-09-27 10:50:39 +0530

    #CommentAdded: Commited some model instance for testing and make association between them

--- prod/users/test/association_test.js
+++ prod/users/test/association_test.js
@@ -4,5 +4,14 @@ const Comment = require("../src/comment");
 const BlogPost = require("../src/blogPost");
 
 describe('Associations', () => {
-    
+    let joe, blogPost, comment;
+
+    beforeEach((done) => {
+        joe = new User({ name: 'joe' });
+        blogPost = new BlogPost({ title: 'JS is Great', content: 'Yep it really is' });
+        comment = new Comment({ content: 'Congrats on a great post' })
+
+        //  AKA Mongoose magic : Mongoose is going to recognize that we just push in an entire model and it's going to automatically say 'OK' they're probably trying to set up an association here, So i'm not going to mess with the entire model itself. I'm just going to set up the 'ObjectId' type to refer to this other blogPost.
+        joe.blogPost.push(blogPost);
+    })
 });
