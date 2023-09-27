4498af2ea2dcf1ed9b491f4fd7700587fd6199e5
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-09-27 11:17:17 +0530

    #CommentAdded: made association between all of the collections

--- prod/users/test/association_test.js
+++ prod/users/test/association_test.js
@@ -12,6 +12,8 @@ describe('Associations', () => {
         comment = new Comment({ content: 'Congrats on a great post' })
 
         //  AKA Mongoose magic : Mongoose is going to recognize that we just push in an entire model and it's going to automatically say 'OK' they're probably trying to set up an association here, So i'm not going to mess with the entire model itself. I'm just going to set up the 'ObjectId' type to refer to this other blogPost.
-        joe.blogPost.push(blogPost);
+        joe.blogPosts.push(blogPost);
+        blogPost.comments.push(comment);
+        comment.user = joe;
     })
 });
