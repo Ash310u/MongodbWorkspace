4cb3a985a3e17a89effc838308585524b9d0f02f
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-09-28 10:22:54 +0530

    Commited a it block, set a assertion for test of 'Saves a full relation graph' and loaded a deeply nested association using 'pupulate, path and model' queries

--- prod/users/test/association_test.js
+++ prod/users/test/association_test.js
@@ -28,5 +28,28 @@ describe('Associations', () => {
                 assert( user.blogPosts[0].title === 'JS is Great')
                 done()
             })
+    });
+
+    it ('Saves a full relation graph', (done) => {
+        User.findOne({ name:'joe' })
+            .populate({
+                // This path key says in the user find the 'blogPosts' property and load up all the associated blogPosts.
+                path:'blogPosts',
+                // Inside of all those 'blogPosts' that you just fetched find the 'comments' property and attempt load up all the associated comments.
+                populate: {
+                    path:'comments',
+                    // Mongoose requires us to tell it which model we want to be using.
+                    model:'comment',
+                    populate: {
+                        path:'user',
+                        model:'user'
+                    }
+                }
+            })
+            .then((user) => {
+                console.log(user.blogPosts[0].comments[0]);
+                assert(user.blogPosts[0].comments[0].content === 'Congrats on a great post')
+                done()
+            })
     })
 }); 
