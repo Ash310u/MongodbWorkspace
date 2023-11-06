3222dcfa882ca5613b60245fadb9943f02c4e3d5
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-11-06 22:08:07 +0530

    Commited all properties in album model and the their types

--- prod/UpStarMusic/database/models/album.js
+++ prod/UpStarMusic/database/models/album.js
@@ -3,5 +3,22 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const AlbumSchema = new Schema({
-    
+    title:{
+        type:String,
+    },
+    date:{
+        type:String,
+    },
+    copiesSold:{
+        type:Number,
+    },
+    numberTracks:{
+        type:Number,
+    },
+    image:{
+        type:String,
+    },
+    revenue:{
+        type:String,
+    },
 })
