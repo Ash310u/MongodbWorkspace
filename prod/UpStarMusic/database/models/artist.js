2632498eb49d778df7bb7e17fa20cd9d70449cbb
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-11-06 21:27:36 +0530

    Commited all properties in artist model the their types

--- prod/UpStarMusic/database/models/artist.js
+++ prod/UpStarMusic/database/models/artist.js
@@ -3,4 +3,34 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema()
 
 const ArtistSchema = new Schema({
+    name:{
+        type:String,
+    },
+    age:{
+        type:Number,
+    },
+    yearsActive:{
+        type:Number,
+    },
+    image:{
+        type:String,
+    },
+    genre:{
+        type:String,
+    },
+    website:{
+        type:String,
+    },
+    netWorth:{
+        type:String,
+    },
+    labelName:{
+        type:String,
+    },
+    retired:{
+        type:Boolean,
+    },
+    album:[{
+        type: Schema.Types.ObjectId,
+    }]
 })
