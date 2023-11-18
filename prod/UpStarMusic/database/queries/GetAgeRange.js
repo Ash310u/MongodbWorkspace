4309ba898ea84a60e583e1d5ddea54b1b8aa2143
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-11-18 10:10:39 +0530

    Commited GetAgeRange and GetYearsActiveRange here i used find() method to get the list of artists and then set a sort() method in ascending and descending order and set the limit to '1' and used .then callback to get the proper field and return a Promise.all([]) with the result of '{ min: result[0], max: result[1] }' because i used Proomise.all() and passed a array of promises thats why i can access the results in a array format

--- prod/UpStarMusic/database/queries/GetAgeRange.js
+++ prod/UpStarMusic/database/queries/GetAgeRange.js
@@ -6,4 +6,22 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+    const minQuery = Artist
+        .find({})
+        .sort({ age: 1 })
+        .limit(1)
+        // find({}) returns a list('[]') of artists but because we are using 'limit(1)' that's why, there is only one item in the array.
+        .then(artists => artists[0].age);
+
+    const maxQuery = Artist
+        .find({})
+        .sort({ age: -1 })
+        .limit(1)
+        .then(artists => artists[0].age);
+
+    return Promise.all([minQuery, maxQuery])
+        .then(result => {
+            return { min: result[0], max: result[1] }
+        })
+
 };
