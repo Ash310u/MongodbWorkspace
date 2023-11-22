fca7bbd3cf8e071b91cfcdd7a800963d1dea232e
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-11-22 15:57:56 +0530

    Commited: Driver model

--- prod/muber/models/driver.js
+++ prod/muber/models/driver.js
@@ -2,5 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DriverSchema = new Schema({
-    
+    email: {
+        type:String,
+        required:true,
+        trim:true
+    },
+    driving: {
+        type:Boolean,
+        default:false,
+    }
 })
+
+const Driver = mongoose.model('driver', DriverSchema);
+
+module.exports = Driver
