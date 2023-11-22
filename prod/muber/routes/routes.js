3a5030eb712bc2d9d3f745cf3edb0056961d1de2
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-11-22 16:01:53 +0530

    Updated: import location

--- prod/muber/routes/routes.js
+++ prod/muber/routes/routes.js
@@ -1,4 +1,4 @@
-const driversControllers = require("../../controllers/drivers_controller")
+const driversControllers = require("../controllers/drivers_controller")
 
 module.exports = (app) => {
     app.get('/api', driversControllers.greeting);
