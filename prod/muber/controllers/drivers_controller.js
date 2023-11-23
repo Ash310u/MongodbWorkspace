6e0e368c890eef9abd22900e71e26c1c6ea22a21
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-11-23 16:18:49 +0530

    Commited: the driver controller saved the driver data to the database and send a res back

--- prod/muber/controllers/drivers_controller.js
+++ prod/muber/controllers/drivers_controller.js
@@ -1,10 +1,16 @@
 const Driver = require("../models/driver")
 
+const greeting = async (req, res) => {
+    res.send({ hi: 'there' })
+}
+
+const create = async (req, res) => {
+    const driver =  new Driver(req.body)
+    await driver.save() 
+    res.status(200).send(driver)
+}
+
 module.exports = {
-    greeting(req, res) {
-        res.send({ hi: 'there' })
-    },
-    create(req, res) {
-        res.send({ hellow: 'internet' })
-    }
+    greeting,
+    create
 }
