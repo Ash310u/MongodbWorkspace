7a8e314f0e3a84cfc9f9da1b9dc78609894c3d01
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-11-24 01:43:19 +0530

    Commited: A test for edit

--- prod/muber/test/controllers/drivers_controller_test.js
+++ prod/muber/test/controllers/drivers_controller_test.js
@@ -6,11 +6,21 @@ const Driver = mongoose.model('driver');
 
 describe('Dirvers controller', () => {
     it('Post to /api/drivers creates a new driver', async () => {
-        const count  = await Driver.countDocuments()
+        const count = await Driver.countDocuments()
         await request(app)
             .post('/api/drivers')
             .send({ email: 'test@mail.com' });
-        const newCount  = await Driver.countDocuments()
+        const newCount = await Driver.countDocuments()
         assert(count + 1 === newCount)
-    })
+    });
+
+    it('Put to /api/drivers/id edits an existing driver', async () => {
+        const driver = new Driver({ email: 'your@mom.com', driving: false })
+        await driver.save()
+        await request(app)
+            .put(`/api/drivers/${driver._id}`)
+            .send({ driving: true })
+        const updatedDriver = await Driver.findOne({ email: 'your@mom.com' })
+        assert(updateDriver.driving === true)
+    });
 })
