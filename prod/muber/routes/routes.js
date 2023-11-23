ed1656576de80093a447a5bbd5e70d60a5595878
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-11-24 01:51:40 +0530

    Commited: A new route for delete request

--- prod/muber/routes/routes.js
+++ prod/muber/routes/routes.js
@@ -4,4 +4,5 @@ module.exports = (app) => {
     app.get('/api', driversControllers.greeting);
     app.post('/api/drivers', driversControllers.create);
     app.put('/api/drivers/:_id', driversControllers.edit);
+    app.delete('/api/drivers/:_id', driversControllers.assDelete);
 }
