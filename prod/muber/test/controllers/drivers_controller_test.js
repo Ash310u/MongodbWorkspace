b8ee18607b311690e3c43ef62f0aad145b2cc68a
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-11-23 15:20:08 +0530

    Commited: a test for '/api/drivers' route and a assertion for the driver is saved or not

--- prod/muber/test/controllers/drivers_controller_test.js
+++ prod/muber/test/controllers/drivers_controller_test.js
@@ -1,11 +1,16 @@
 const assert = require('assert')
+const mongoose = require('mongoose')
 const request = require('supertest')
 const app = require('../../app')
+const Driver = mongoose.model('driver');
 
 describe('Dirvers controller', () => {
     it('Post to /api/drivers creates a new driver', async () => {
-        const response = await request(app)
+        const count  = await Driver.countDocuments()
+        await request(app)
             .post('/api/drivers')
-            .send({ email: 'test@mail.com' })
+            .send({ email: 'test@mail.com' });
+        const newCount  = await Driver.countDocuments()
+        assert(count + 1 === newCount)
     })
 })
