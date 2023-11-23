07e0262f049ac9f77387ff604bf68a80f6bd8af1
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-11-23 15:17:03 +0530

    Commited: A connection to the mongo database

--- prod/muber/app.js
+++ prod/muber/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const routes = require('./routes/routes');
 const app = express();
+
+mongoose.Promise = global.Promise
+
+mongoose.connect('mongodb://127.0.0.1/muber')
 
 app.use(express.json())
 routes(app)
