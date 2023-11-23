65dfe5ac25a37af5a549ad07aa2438a721ae92f5
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-11-24 01:23:46 +0530

    Commited: A new route for put request

--- prod/muber/routes/routes.js
+++ prod/muber/routes/routes.js
@@ -3,4 +3,5 @@ const driversControllers = require("../controllers/drivers_controller")
 module.exports = (app) => {
     app.get('/api', driversControllers.greeting);
     app.post('/api/drivers', driversControllers.create);
+    app.put('/api/drivers/:_id', driversControllers.edit);
 }
